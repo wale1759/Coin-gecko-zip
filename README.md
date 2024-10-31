fdc66908181208e3eb3a01130f4a7766a314f9ce
Olawale <sanmiafolabi1759@gmail.com>
2024-11-01 06:58:15 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Coin-gecko-zip
+ 
---
+++ src/components/about/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './about.css'
+
+const About = () => {
+  return (
+    <div>
+   <p>Just testing coin Gecko Api</p>
+    </div>
+  )
+}
+
+export default About
